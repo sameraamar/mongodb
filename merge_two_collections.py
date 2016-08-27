11dee282f9abd286c1ce377292938d71ad17937a
sameraamar <samer.aamar@gmail.com>
2016-08-27 09:55:00 +0300

    adding an option of running in bulk
    adding an option of running in bulk

--- merge_two_collections.py
+++ merge_two_collections.py
@@ -10,6 +10,7 @@ merge two mongodb collections
 """
 
 from pymongo import MongoClient
+from pymongo.errors import BulkWriteError
 
 
 host = 'localhost'
@@ -19,8 +20,8 @@ client = MongoClient(host, int(port))
 src_dbname = 'twitter_db'
 trgt_dbname = 'twitter_db'
 
-src_coll = '2016-08-24-2016-08-25'
-trgt_coll = '2016-08-23-2016-08-25'
+src_coll = '2016-08-23-2016-08-25'
+trgt_coll = '2016-08-23-2016-08-24'
 
 
 src_db = client[src_dbname]
@@ -30,24 +31,64 @@ trgt_db = client[trgt_dbname]
 src = src_db[src_coll]
 trgt = trgt_db[trgt_coll]
 
-print('Source: ', src.count(), ' target: ', trgt.count())        
+trgt_count = trgt.count()
+print('Source: ', src.count(), ' target: ', trgt_count)        
 
 cursor = src.find({})
 c = 0
+inserts = 0
+duplicates = 0
+
+bulk = None
 for s in cursor:
     c+=1
-    res = trgt.find({'_id': s['_id']})
-    if res.count() == 0:
-        trgt.insert_one(s)
     
-    perc = 100.0*c/src.count()
-    ruff = int(5.0/100*src.count())
+    if bulk==None:
+        bulk = src.initialize_unordered_bulk_op()
     
-    if c % ruff==0:
-        print('Handled (', c ,') our of (',src.count(),') which is %', perc)
+#    res = bulk.find({'_id': s['_id']})
+#    if res.count() == 0:
+#        bulk.insert(s)
+#        inserts+=1
+#    else:
+#        duplicates+=1
+
+ 
+    bulk.insert(s)
+
+
+#    res = trgt.find({'_id': s['_id']})
+#    if res.count() == 0:
+#        trgt.insert_one(s)
+#        inserts+=1
+#    else:
+#        duplicates+=1
+    
+    #ruff = int(5.0/100*src.count())
+    
+    if c % 2500==0:
+        perc = 100.0*c/src.count()
+        try: 
+            result = bulk.execute()
+            inserts += 2500
+        except BulkWriteError as bwe:
+            inserts += bwe.details['nInserted']
+            duplicates += len(bwe.details['writeErrors'])
+        bulk = None
+        
+        print('Handled (', c ,') out of (',src.count(),') which is %', perc)
         #res.remove()
 
-print('Source: ', src.count(), ' target: ', trgt.count())        
+if bulk != None:
+    try: 
+        result = bulk.execute()
+        inserts += 1000
+    except BulkWriteError as bwe:
+        inserts += bwe.details['nInserted']
+        duplicates += len(bwe.details['writeErrors'])
+
+print('Source: ', src.count(), ' target: ', trgt.count(), '. was (',trgt_count,')')     
+print('Inserted: ', inserts, ', duplicates: ', duplicates)   
 print('You can safely remove ', src_dbname,'.', src_coll)
 
 
